2607ac17e6a811c7eb6e8353a371dce4f3c2f371
shtaxxx <shta.ky1018@gmail.com>
2015-08-25 01:37:51 +0900

    While staement is supported.

---
+++ sample/test/while/led.py
@@ -0,0 +1,39 @@
+import sys
+import os
+
+from veriloggen import *
+
+def mkTest():
+    m = Module('test')
+    clk = m.Reg('CLK')
+    rst = m.Reg('RST')
+    count = m.Reg('count', width=32)
+
+    m.Initial(
+        clk(0),
+        Forever(clk(Not(clk), ldelay=5)) # forever #5 CLK = ~CLK;
+    )
+
+    m.Initial(
+        rst(0),
+        Delay(100),
+        rst(1),
+        Delay(100),
+        rst(0),
+        Delay(1000),
+
+        count(0),
+        
+        While(count < 1024)(
+            count( count + 1 )
+        ),
+        
+        Systask('finish'),
+    )
+
+    return m
+
+if __name__ == '__main__':
+    test = mkTest()
+    verilog = test.to_verilog('tmp.v')
+    print(verilog)
---
+++ sample/test/while/test_led.py
@@ -0,0 +1,45 @@
+import led
+
+expected_verilog = """
+module test;
+  reg CLK;
+  reg RST; 
+  reg [32-1:0] count;
+
+  initial begin
+    CLK = 0;
+    forever begin
+      #5 CLK = !CLK;
+    end
+  end
+
+  initial begin
+    RST = 0;
+    #100;
+    RST = 1;
+    #100;
+    RST = 0;
+    #1000;
+
+    count = 0;
+    while(count < 1024) begin
+      count = count + 1;
+    end
+
+    $finish;
+  end
+endmodule
+"""
+
+def test_led():
+    test_module = led.mkTest()
+    test_code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    parser = VerilogParser()
+    expected_ast = parser.parse(expected_verilog)
+    codegen = ASTCodeGenerator()
+    expected_code = codegen.visit(expected_ast)
+
+    assert(expected_code == test_code)
