cd22eb30473bf9a8bbaba2d2dc2ae393ade21a0e
shtaxxx <shta.ky1018@gmail.com>
2015-11-02 14:06:37 +0900

    lib_simulation_simulator is added: executing a simulation of Veriloggen hardware module on Python with Icarus.

---
+++ tests/lib_simulation_/simulator/lib_simulation_simulator.py
@@ -0,0 +1,68 @@
+import sys
+import os
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+from veriloggen import *
+
+def mkLed():
+    m = Module('blinkled')
+    width = m.Parameter('WIDTH', 8)
+    clk = m.Input('CLK')
+    rst = m.Input('RST')
+    led = m.OutputReg('LED', width)
+    count = m.Reg('count', 32)
+
+    m.Always(Posedge(clk))(
+        If(rst)(
+            count(0)
+        ).Else(
+            If(count == 16 - 1)(
+                count(0)
+            ).Else(
+                count(count + 1)
+            )
+        ))
+    
+    m.Always(Posedge(clk))(
+        If(rst)(
+            led(0)
+        ).Else(
+            Systask('display', 'LED:%d count:%d', led, count),
+            If(count == 16 - 1)(
+                led(led + 1)
+            )
+        ))
+    
+    return m
+
+def mkTest():
+    m = Module('test')
+    width = m.Parameter('WIDTH', 8)
+    clk = m.Reg('CLK')
+    rst = m.Reg('RST')
+    led = m.Wire('LED', width)
+
+    uut = m.Instance(mkLed(), 'uut',
+                     params=connect_same_name(width),
+                     ports=connect_same_name(clk, rst, led))
+
+    lib.simulation.setup_waveform(m, uut)
+    lib.simulation.setup_clock(m, clk, hperiod=5)
+    init = lib.simulation.setup_reset(m, rst, period=100)
+
+    init.add(
+        Delay(1000),
+        Systask('finish'),
+    )
+
+    return m
+
+if __name__ == '__main__':
+    test = mkTest()
+    verilog = test.to_verilog()
+    print(verilog)
+    sim = lib.simulation.Simulator(test)
+    rslt = sim.run()
+    print(rslt)
