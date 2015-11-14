508a6d158b2bd016da294bbfe3f8436aef9ec148
shtaxxx <shta.ky1018@gmail.com>
2015-11-14 20:05:58 +0900

    lib_fsm_goto_from is added: lib.FSM supports goto_from() that append a state movement later.

---
+++ tests/lib_fsm_/goto_from/test_lib_fsm_goto_from.py
@@ -0,0 +1,85 @@
+import lib_fsm_goto_from
+
+expected_verilog = """
+module blinkled #
+  (
+   parameter WIDTH = 8
+   )
+  (
+   input CLK, 
+   input RST, 
+   output reg [WIDTH-1:0] LED
+   );
+  reg [32-1:0] count;
+  reg [32-1:0] fsm;
+  localparam fsm_init = 0;
+  localparam fsm_1 = 1;
+  localparam fsm_2 = 2;
+  localparam fsm_3 = 3;
+  localparam fsm_4 = 4;
+  localparam fsm_5 = 5;
+  localparam fsm_6 = 6;
+  localparam fsm_7 = 7;
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      count <= 0;
+      LED <= 0;
+      fsm <= fsm_init;
+    end else begin
+      case(fsm)
+        fsm_init: begin
+          LED <= 0;
+          fsm <= fsm_1;
+        end
+        fsm_1: begin
+          count <= 0;
+          fsm <= fsm_2;
+        end
+        fsm_2: begin
+          if(count < 1024) begin
+            fsm <= fsm_3;
+          end 
+          if(!(count < 1024)) begin
+            fsm <= fsm_7;
+          end 
+        end
+        fsm_3: begin
+          count <= count + 1;
+          fsm <= fsm_4;
+        end
+        fsm_4: begin
+          count <= count + 1;
+          fsm <= fsm_5;
+        end
+        fsm_5: begin
+          count <= count + 1;
+          fsm <= fsm_6;
+        end
+        fsm_6: begin
+          count <= count + 1;
+          fsm <= fsm_2;
+        end
+        fsm_7: begin
+          LED <= LED + 1;
+          fsm <= fsm_1;
+        end
+      endcase
+    end
+  end
+
+endmodule
+"""
+
+def test():
+    test_module = lib_fsm_goto_from.mkLed()
+    code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    parser = VerilogParser()
+    expected_ast = parser.parse(expected_verilog)
+    codegen = ASTCodeGenerator()
+    expected_code = codegen.visit(expected_ast)
+
+    assert(expected_code == code)
