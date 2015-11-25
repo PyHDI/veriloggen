72cc229eed1a7f0e1a514da70d07deaa9262f344
shtaxxx <shta.ky1018@gmail.com>
2015-11-25 18:34:20 +0900

    lib_bundle is removed, because it is not useful anymore.

--- veriloggen/lib/__init__.py
+++ veriloggen/lib/__init__.py
@@ -1,19 +1,15 @@
 #-------------------------------------------------------------------------------
 # lib/__init__.py
 #
-# Veriloggen library
+# Veriloggen Library
 # 
 # Copyright (C) 2015, Shinya Takamaeda-Yamazaki
 # License: Apache 2.0
 #-------------------------------------------------------------------------------
 from __future__ import absolute_import
 from __future__ import print_function
-import os
-import sys
 
-## Please add import notations here for additional library
-from .fsm import FSM
 from .seq import Seq
+from .fsm import FSM
 from .dataflow import Dataflow
-from .bundle import Bundle
 from . import simulation
