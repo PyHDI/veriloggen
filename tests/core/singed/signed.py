086550851b920e1c21466b407147a34394f9c8ce
shtaxxx <shta.ky1018@gmail.com>
2016-01-17 02:34:38 +0900

    tests/core/signed is added: signed port is supported. Pyverilog is also updated.

---
+++ tests/core/singed/signed.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+import os
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+from veriloggen import *
+
+def mkLed():
+    m = Module('blinkled')
+    width = m.Parameter('WIDTH', 8)
+    clk = m.Input('CLK')
+    rst = m.Input('RST')
+    val = m.Input('VAL', width, signed=True)
+    led = m.OutputReg('LED', width, signed=True)
+    count = m.Reg('count', 32, signed=True)
+
+    m.Always(Posedge(clk))(
+        If(rst)(
+            count(0)
+        ).Else(
+            If(count == 1023)(
+                count(0)
+            ).Else(
+                count(count + 1)
+            )
+        ))
+    
+    m.Always(Posedge(clk))(
+        If(rst)(
+            led( Int(0b00000001, width=8, base=2) )
+        ).Else(
+            If(count == 1023)(
+                led( led + 1 )
+            )
+        ))
+
+    return m
+
+if __name__ == '__main__':
+    led = mkLed()
+    verilog = led.to_verilog()
+    print(verilog)
---
+++ tests/core/singed/test_signed.py
@@ -0,0 +1,55 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import signed
+
+expected_verilog = """
+module blinkled #
+(
+  parameter WIDTH = 8
+)
+(
+  input CLK,
+  input RST,
+  input signed [WIDTH-1:0] VAL,
+  output reg signed [WIDTH-1:0] LED
+);
+
+  reg signed [32-1:0] count;
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      count <= 0;
+    end else begin
+      if(count == 1023) begin
+        count <= 0;
+      end else begin
+        count <= count + 1;
+      end
+    end
+  end
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      LED <= 8'b1;
+    end else begin
+      if(count == 1023) begin
+        LED <= LED + 1;
+      end 
+    end
+  end
+
+endmodule
+"""
+
+def test():
+    test_module = signed.mkLed()
+    code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    parser = VerilogParser()
+    expected_ast = parser.parse(expected_verilog)
+    codegen = ASTCodeGenerator()
+    expected_code = codegen.visit(expected_ast)
+
+    assert(expected_code == code)
