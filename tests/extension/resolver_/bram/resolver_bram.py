aea22eebe781adf53c1fb57874d212893f2893fa
shtaxxx <shta.ky1018@gmail.com>
2016-04-20 19:02:50 +0900

    bram example is added.

---
+++ tests/extension/resolver_/bram/resolver_bram.py
@@ -0,0 +1,98 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+import os
+import math
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))
+
+from veriloggen import *
+
+def mkBram(datawidth=32, addrwidth=10, numports=2):
+    m = Module('BRAM%d' % numports)
+
+    datawidth = m.Parameter('DATA_WIDTH', datawidth)
+    addrwidth = m.Parameter('ADDR_WIDTH', addrwidth)
+
+    clk = m.Input('CLK')
+    ports = []
+    for i in range(numports):
+        addr = m.Input('ADDR%d' % i, addrwidth)
+        din = m.Input('DIN%d' % i, datawidth)
+        we = m.Input('WE%d' % i)
+        dout = m.Output('DOUT%d' % i, datawidth)
+        delay_addr = m.Reg('delay_ADDR%d' % i, addrwidth)
+        ports.append( (addr, din, we, dout, delay_addr) )
+
+    mem = m.Reg('mem', datawidth, length=Int(2)**addrwidth)
+
+    for i in range(numports):
+        addr, din ,we, dout, delay_addr = ports[i]
+        m.Always(Posedge(clk))(
+            If(we)(
+                mem[addr](din)
+            ),
+            delay_addr(addr)
+        )
+        m.Assign(dout(mem[delay_addr]))
+
+    return m
+
+def mkMain(n=128, datawidth=32, numports=2):
+    m = Module('main')
+
+    clk = m.Input('CLK')
+    rst = m.Input('RST')
+
+    addrwidth = int(math.log(n, 2)) * 2
+
+    datawidth = m.Parameter('DATA_WIDTH', datawidth)
+    addrwidth = m.Parameter('ADDR_WIDTH', addrwidth)
+    
+    bram = mkBram(datawidth, addrwidth, numports)
+
+    bram_ports = []
+    for i in range(numports):
+        addr = m.Reg('bram_addr%d' % i, addrwidth)
+        din = m.Reg('bram_din%d' % i, datawidth)
+        we = m.Reg('bram_we%d' % i)
+        dout = m.Wire('bram_dout%d' % i, datawidth)
+        bram_ports.append( [addr, din, we, dout] )
+
+    params = [ (datawidth.name, datawidth), 
+               (addrwidth.name, addrwidth) ]
+    ports = [ clk ]
+    for bram_port in bram_ports:
+        ports.extend(bram_port)
+
+    # BRAM instance
+    m.Instance(bram, 'bram_inst', params=params, ports=ports)
+
+    # example how to access BRAM
+    fsm = FSM(m, 'fsm', clk, rst)
+    
+    for addr, din, we, dout in bram_ports:
+        fsm.add( addr(0), din(0), we(0) )
+
+    fsm.goto_next()
+
+    count = m.Reg('count', 32, initval=0)
+    for port, (addr, din, we, dout) in enumerate(bram_ports):
+        fsm.add( addr(count+port*n//numports), din(count), we(1), count.inc() )
+
+    fsm.goto_next(cond=count==n//numports-1)
+    
+    fsm.make_always()
+    
+    return m
+
+def mkOrig():
+    m = mkMain()
+    m = resolver.resolve(m)
+    return m
+    
+if __name__ == '__main__':
+    orig = mkOrig()
+    verilog = orig.to_verilog()
+    print(verilog)
---
+++ tests/extension/resolver_/bram/test_resolver_bram.py
@@ -0,0 +1,139 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import resolver_bram
+
+expected_verilog = """
+module main #
+(
+  parameter DATA_WIDTH = 32,
+  parameter ADDR_WIDTH = 14
+)
+(
+  input CLK,
+  input RST
+);
+
+  reg [14-1:0] bram_addr0;
+  reg [32-1:0] bram_din0;
+  reg bram_we0;
+  wire [32-1:0] bram_dout0;
+  reg [14-1:0] bram_addr1;
+  reg [32-1:0] bram_din1;
+  reg bram_we1;
+  wire [32-1:0] bram_dout1;
+
+  BRAM2
+  #(
+    .DATA_WIDTH(32),
+    .ADDR_WIDTH(14)
+  )
+  bram_inst
+  (
+    .CLK(CLK),
+    .ADDR0(bram_addr0),
+    .DIN0(bram_din0),
+    .WE0(bram_we0),
+    .DOUT0(bram_dout0),
+    .ADDR1(bram_addr1),
+    .DIN1(bram_din1),
+    .WE1(bram_we1),
+    .DOUT1(bram_dout1)
+  );
+
+  reg [32-1:0] fsm;
+  localparam fsm_init = 0;
+  reg [32-1:0] count;
+  localparam fsm_1 = 1;
+  localparam fsm_2 = 2;
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      fsm <= 0;
+      count <= 0;
+    end else begin
+      case(fsm)
+        0: begin
+          bram_addr0 <= 0;
+          bram_din0 <= 0;
+          bram_we0 <= 0;
+          bram_addr1 <= 0;
+          bram_din1 <= 0;
+          bram_we1 <= 0;
+          fsm <= 1;
+        end
+        1: begin
+          bram_addr0 <= count + 0;
+          bram_din0 <= count;
+          bram_we0 <= 1;
+          count <= count + 1;
+          bram_addr1 <= count + 64;
+          bram_din1 <= count;
+          bram_we1 <= 1;
+          count <= count + 1;
+          if(count == 63) begin
+            fsm <= 2;
+          end 
+        end
+      endcase
+    end
+  end
+
+
+endmodule
+
+
+
+module BRAM2 #
+(
+  parameter DATA_WIDTH = 32,
+  parameter ADDR_WIDTH = 14
+)
+(
+  input CLK,
+  input [14-1:0] ADDR0,
+  input [32-1:0] DIN0,
+  input WE0,
+  output [32-1:0] DOUT0,
+  input [14-1:0] ADDR1,
+  input [32-1:0] DIN1,
+  input WE1,
+  output [32-1:0] DOUT1
+);
+
+  reg [14-1:0] delay_ADDR0;
+  reg [14-1:0] delay_ADDR1;
+  reg [32-1:0] mem [0:2**ADDR_WIDTH-1];
+
+  always @(posedge CLK) begin
+    if(WE0) begin
+      mem[ADDR0] <= DIN0;
+    end 
+    delay_ADDR0 <= ADDR0;
+  end
+
+  assign DOUT0 = mem[delay_ADDR0];
+
+  always @(posedge CLK) begin
+    if(WE1) begin
+      mem[ADDR1] <= DIN1;
+    end 
+    delay_ADDR1 <= ADDR1;
+  end
+
+  assign DOUT1 = mem[delay_ADDR1];
+
+endmodule
+"""
+
+def test():
+    test_module = resolver_bram.mkOrig()
+    code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    parser = VerilogParser()
+    expected_ast = parser.parse(expected_verilog)
+    codegen = ASTCodeGenerator()
+    expected_code = codegen.visit(expected_ast)
+
+    assert(expected_code == code)
