77377bd589a1436dcd6aa01c8ec8426b725f8de0
shtaxxx <shta.ky1018@gmail.com>
2016-04-21 15:22:02 +0900

    Test cases named as mux_const are added for testing Mux with a constant condition.

---
+++ tests/core/mux_const/test_mux_const.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import mux_const
+
+expected_verilog = """
+module blinkled #
+  ( 
+   parameter WIDTH = 8
+  )
+  ( 
+   input CLK, 
+   input RST,
+   output reg [WIDTH-1:0] LED
+  );
+  reg [32-1:0] count;
+  always @(posedge CLK) begin
+    if(RST) begin
+      count <= 0;
+    end else begin
+      count <= count + 1;
+    end
+  end
+  always @(posedge CLK) begin
+    if(RST) begin
+      LED <= 0;
+    end else begin
+      LED <= LED + 1;
+    end
+  end
+endmodule
+"""
+
+def test():
+    test_module = mux_const.mkLed()
+    code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    parser = VerilogParser()
+    expected_ast = parser.parse(expected_verilog)
+    codegen = ASTCodeGenerator()
+    expected_code = codegen.visit(expected_ast)
+
+    assert(expected_code == code)
