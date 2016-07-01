77962a5376fd700847e434926e4ef659dff5d706
shtaxxx <shta.ky1018@gmail.com>
2016-07-01 17:14:33 +0900

    Type swap util in types

--- veriloggen/types/util.py
+++ veriloggen/types/util.py
@@ -3,6 +3,15 @@ from __future__ import print_function
 
 import veriloggen.core.vtypes as vtypes
 
+t_Input = 'Input'
+t_Output = 'Output'
+t_Wire = 'Wire'
+t_Reg = 'Reg'
+t_OutputReg = 'OutputReg'
+
+def swap_type(cls):
+    return cls._O, cls._I
+
 def make_port(m, _type, *args, **kwargs):
     if 'initval' in kwargs and 'Reg' not in _type:
         del kwargs['initval']
