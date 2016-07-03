daf07f9d708701e6cf4e79ffb57d9a7497b0736b
shtaxxx <shta.ky1018@gmail.com>
2016-07-04 01:06:36 +0900

    AXI interface is added. Currently only write operation is implemented.

---
+++ tests/extension/types_/axi_/fsm_write/test_types_axi_fsm_write.py
@@ -0,0 +1,309 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import types_axi_fsm_write
+
+expected_verilog = """
+module test;
+
+  reg CLK;
+  reg RST;
+  wire [32-1:0] myaxi_awaddr;
+  wire [8-1:0] myaxi_awlen;
+  wire myaxi_awvalid;
+  reg myaxi_awready;
+  wire [32-1:0] myaxi_wdata;
+  wire [4.0-1:0] myaxi_wstrb;
+  wire myaxi_wlast;
+  wire myaxi_wvalid;
+  reg myaxi_wready;
+  wire [32-1:0] myaxi_araddr;
+  wire [8-1:0] myaxi_arlen;
+  wire myaxi_arvalid;
+  reg myaxi_arready;
+  reg [32-1:0] myaxi_rdata;
+  reg myaxi_rlast;
+  reg myaxi_rvalid;
+  wire myaxi_rready;
+  reg [32-1:0] waddr;
+  localparam waddr_init = 0;
+  localparam waddr_1 = 1;
+  localparam waddr_2 = 2;
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      waddr <= waddr_init;
+    end else begin
+      case(waddr)
+        waddr_init: begin
+          myaxi_awready <= 0;
+          if(myaxi_awvalid) begin
+            waddr <= waddr_1;
+          end 
+        end
+        waddr_1: begin
+          if(myaxi_awvalid) begin
+            myaxi_awready <= 1;
+          end 
+          waddr <= waddr_2;
+        end
+        waddr_2: begin
+          myaxi_awready <= 0;
+          waddr <= waddr_init;
+        end
+      endcase
+    end
+  end
+
+  reg [32-1:0] wdata;
+  localparam wdata_init = 0;
+  localparam wdata_1 = 1;
+  localparam wdata_2 = 2;
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      wdata <= wdata_init;
+    end else begin
+      case(wdata)
+        wdata_init: begin
+          myaxi_wready <= 0;
+          if(myaxi_wvalid) begin
+            wdata <= wdata_1;
+          end 
+        end
+        wdata_1: begin
+          if(myaxi_wvalid) begin
+            myaxi_wready <= 1;
+          end 
+          wdata <= wdata_2;
+        end
+        wdata_2: begin
+          myaxi_wready <= 0;
+          wdata <= wdata_init;
+        end
+      endcase
+    end
+  end
+
+  wire _tmp_0;
+  assign _tmp_0 = 0;
+
+  always @(*) begin
+    myaxi_arready <= _tmp_0;
+  end
+
+  wire _tmp_1;
+  assign _tmp_1 = 0;
+
+  always @(*) begin
+    myaxi_rvalid <= _tmp_1;
+  end
+
+  wire [32-1:0] _tmp_2;
+  assign _tmp_2 = 0;
+
+  always @(*) begin
+    myaxi_rdata <= _tmp_2;
+  end
+
+  wire _tmp_3;
+  assign _tmp_3 = 0;
+
+  always @(*) begin
+    myaxi_rlast <= _tmp_3;
+  end
+
+
+  main
+  uut
+  (
+    .CLK(CLK),
+    .RST(RST),
+    .myaxi_awaddr(myaxi_awaddr),
+    .myaxi_awlen(myaxi_awlen),
+    .myaxi_awvalid(myaxi_awvalid),
+    .myaxi_awready(myaxi_awready),
+    .myaxi_wdata(myaxi_wdata),
+    .myaxi_wstrb(myaxi_wstrb),
+    .myaxi_wlast(myaxi_wlast),
+    .myaxi_wvalid(myaxi_wvalid),
+    .myaxi_wready(myaxi_wready),
+    .myaxi_araddr(myaxi_araddr),
+    .myaxi_arlen(myaxi_arlen),
+    .myaxi_arvalid(myaxi_arvalid),
+    .myaxi_arready(myaxi_arready),
+    .myaxi_rdata(myaxi_rdata),
+    .myaxi_rlast(myaxi_rlast),
+    .myaxi_rvalid(myaxi_rvalid),
+    .myaxi_rready(myaxi_rready)
+  );
+
+
+  initial begin
+    $dumpfile("uut.vcd");
+    $dumpvars(0, uut, CLK, RST, myaxi_awaddr, myaxi_awlen, myaxi_awvalid, myaxi_awready, myaxi_wdata, myaxi_wstrb, myaxi_wlast, myaxi_wvalid, myaxi_wready, myaxi_araddr, myaxi_arlen, myaxi_arvalid, myaxi_arready, myaxi_rdata, myaxi_rlast, myaxi_rvalid, myaxi_rready, waddr, wdata, _tmp_0, _tmp_1, _tmp_2, _tmp_3);
+  end
+
+
+  initial begin
+    CLK = 0;
+    forever begin
+      #5 CLK = !CLK;
+    end
+  end
+
+
+  initial begin
+    RST = 0;
+    waddr = waddr_init;
+    wdata = wdata_init;
+    #100;
+    RST = 1;
+    #100;
+    RST = 0;
+    #100000;
+    $finish;
+  end
+
+
+endmodule
+
+
+
+module main
+(
+  input CLK,
+  input RST,
+  output reg [32-1:0] myaxi_awaddr,
+  output reg [8-1:0] myaxi_awlen,
+  output reg myaxi_awvalid,
+  input myaxi_awready,
+  output reg [32-1:0] myaxi_wdata,
+  output reg [4.0-1:0] myaxi_wstrb,
+  output reg myaxi_wlast,
+  output reg myaxi_wvalid,
+  input myaxi_wready,
+  output reg [32-1:0] myaxi_araddr,
+  output reg [8-1:0] myaxi_arlen,
+  output reg myaxi_arvalid,
+  input myaxi_arready,
+  input [32-1:0] myaxi_rdata,
+  input myaxi_rlast,
+  input myaxi_rvalid,
+  output reg myaxi_rready
+);
+
+  reg [32-1:0] fsm;
+  localparam fsm_init = 0;
+  reg [8-1:0] _tmp_0;
+  reg _myaxi_cond_0_1;
+  reg [32-1:0] wdata;
+  reg _tmp_1;
+  reg _myaxi_cond_1_1;
+  reg _myaxi_cond_2_1;
+  localparam fsm_1 = 1;
+  localparam fsm_2 = 2;
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      fsm <= fsm_init;
+      wdata <= 0;
+    end else begin
+      case(fsm)
+        fsm_init: begin
+          if(myaxi_awready || !myaxi_awvalid) begin
+            fsm <= fsm_1;
+          end 
+        end
+        fsm_1: begin
+          if(myaxi_wready || !myaxi_wvalid) begin
+            wdata <= wdata + 1;
+          end 
+          if((myaxi_wready || !myaxi_wvalid) && _tmp_1) begin
+            fsm <= fsm_2;
+          end 
+        end
+      endcase
+    end
+  end
+
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      myaxi_araddr <= 0;
+      myaxi_arlen <= 0;
+      myaxi_arvalid <= 0;
+      myaxi_rready <= 0;
+      myaxi_awaddr <= 0;
+      myaxi_awlen <= 0;
+      myaxi_awvalid <= 0;
+      _tmp_0 <= 0;
+      _myaxi_cond_0_1 <= 0;
+      myaxi_wdata <= 0;
+      myaxi_wvalid <= 0;
+      myaxi_wlast <= 0;
+      myaxi_wstrb <= 0;
+      _tmp_1 <= 0;
+      _myaxi_cond_1_1 <= 0;
+      _myaxi_cond_2_1 <= 0;
+    end else begin
+      if(_myaxi_cond_0_1) begin
+        myaxi_awvalid <= 0;
+      end 
+      if(_myaxi_cond_1_1) begin
+        myaxi_wvalid <= 0;
+      end 
+      if(_myaxi_cond_2_1) begin
+        myaxi_wlast <= 0;
+        _tmp_1 <= 0;
+      end 
+      myaxi_araddr <= 0;
+      myaxi_arlen <= 0;
+      myaxi_arvalid <= 0;
+      myaxi_rready <= 0;
+      if((fsm == 0) && (myaxi_awready || !myaxi_awvalid)) begin
+        myaxi_awaddr <= 1024;
+        myaxi_awlen <= 63;
+        myaxi_awvalid <= 1;
+        _tmp_0 <= 63;
+      end 
+      _myaxi_cond_0_1 <= 1;
+      if(myaxi_awvalid && !myaxi_awready) begin
+        myaxi_awvalid <= myaxi_awvalid;
+      end 
+      if((fsm == 1) && ((myaxi_wready || !myaxi_wvalid) && !_tmp_1)) begin
+        myaxi_wdata <= wdata;
+        myaxi_wvalid <= 1;
+        myaxi_wlast <= 0;
+        myaxi_wstrb <= { 4{ 1'd1 } };
+        _tmp_0 <= _tmp_0 - 1;
+      end 
+      if((fsm == 1) && ((myaxi_wready || !myaxi_wvalid) && !_tmp_1) && (_tmp_0 == 0)) begin
+        myaxi_wlast <= 1;
+        _tmp_1 <= 1;
+      end 
+      _myaxi_cond_1_1 <= 1;
+      _myaxi_cond_2_1 <= 1;
+      if(myaxi_wvalid && !myaxi_wready) begin
+        myaxi_wvalid <= myaxi_wvalid;
+        myaxi_wlast <= myaxi_wlast;
+        _tmp_1 <= _tmp_1;
+      end 
+    end
+  end
+
+
+endmodule
+"""
+
+def test():
+    test_module = types_axi_fsm_write.mkTest()
+    code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    parser = VerilogParser()
+    expected_ast = parser.parse(expected_verilog)
+    codegen = ASTCodeGenerator()
+    expected_code = codegen.visit(expected_ast)
+
+    assert(expected_code == code)
---
+++ veriloggen/types/axi.py
@@ -0,0 +1,273 @@
+from __future__ import absolute_import
+from __future__ import print_function
+
+import veriloggen.core.vtypes as vtypes
+import veriloggen.core.module as module
+from veriloggen.seq.seq import Seq
+from . import util
+
+
+class AxiBase(object):
+    _I = util.t_Input
+    _O = util.t_OutputReg
+
+    def __init__(self, m, name=None, datawidth=32, addrwidth=32, itype=None, otype=None):
+        if itype is None:
+            itype = self._I
+        if otype is None:
+            otype = self._O
+        self.m = m
+        self.datawidth = datawidth
+        self.addrwidth = addrwidth
+        self.itype = itype
+        self.otype = otype
+
+
+class AxiWriteAddress(AxiBase):
+
+    def __init__(self, m, name=None, datawidth=32, addrwidth=32, itype=None, otype=None):
+        AxiBase.__init__(self, m, name, datawidth, addrwidth, itype, otype)
+        self.awaddr = util.make_port(
+            m, self.otype, name + '_awaddr', self.addrwidth, initval=0)
+        self.awlen = util.make_port(
+            m, self.otype, name + '_awlen', 8, initval=0)
+        self.awvalid = util.make_port(
+            m, self.otype, name + '_awvalid', None, initval=0)
+        self.awready = util.make_port(
+            m, self.itype, name + '_awready', None, initval=0)
+
+
+class AxiWriteData(AxiBase):
+
+    def __init__(self, m, name=None, datawidth=32, addrwidth=32, itype=None, otype=None):
+        AxiBase.__init__(self, m, name, datawidth, addrwidth, itype, otype)
+        self.wdata = util.make_port(
+            m, self.otype, name + '_wdata', self.datawidth, initval=0)
+        self.wstrb = util.make_port(
+            m, self.otype, name + '_wstrb', self.datawidth / 8, initval=0)
+        self.wlast = util.make_port(
+            m, self.otype, name + '_wlast', None, initval=0)
+        self.wvalid = util.make_port(
+            m, self.otype, name + '_wvalid', None, initval=0)
+        self.wready = util.make_port(
+            m, self.itype, name + '_wready', None, initval=0)
+
+
+class AxiReadAddress(AxiBase):
+
+    def __init__(self, m, name=None, datawidth=32, addrwidth=32, itype=None, otype=None):
+        AxiBase.__init__(self, m, name, datawidth, addrwidth, itype, otype)
+        self.araddr = util.make_port(
+            m, self.otype, name + '_araddr', self.addrwidth, initval=0)
+        self.arlen = util.make_port(
+            m, self.otype, name + '_arlen', 8, initval=0)
+        self.arvalid = util.make_port(
+            m, self.otype, name + '_arvalid', None, initval=0)
+        self.arready = util.make_port(
+            m, self.itype, name + '_arready', None, initval=0)
+
+
+class AxiReadData(AxiBase):
+
+    def __init__(self, m, name=None, datawidth=32, addrwidth=32, itype=None, otype=None):
+        AxiBase.__init__(self, m, name, datawidth, addrwidth, itype, otype)
+        self.rdata = util.make_port(
+            m, self.itype, name + '_rdata', self.datawidth, initval=0)
+        self.rlast = util.make_port(
+            m, self.itype, name + '_rlast', None, initval=0)
+        self.rvalid = util.make_port(
+            m, self.itype, name + '_rvalid', None, initval=0)
+        self.rready = util.make_port(
+            m, self.otype, name + '_rready', None, initval=0)
+
+        
+# master port
+class AxiMasterWriteAddress(AxiWriteAddress):
+    pass
+
+
+class AxiMasterWriteData(AxiWriteData):
+    pass
+
+
+class AxiMasterReadAddress(AxiReadAddress):
+    pass
+
+
+class AxiMasterReadData(AxiReadData):
+    pass
+
+
+# slave port
+class AxiSlaveWriteAddress(AxiWriteAddress):
+    _I, _O = util.swap_type(AxiWriteAddress)
+
+
+class AxiSlaveWriteData(AxiWriteData):
+    _I, _O = util.swap_type(AxiWriteData)
+
+
+class AxiSlaveReadAddress(AxiReadAddress):
+    _I, _O = util.swap_type(AxiReadAddress)
+
+
+class AxiSlaveReadData(AxiReadData):
+    _I, _O = util.swap_type(AxiReadData)
+
+    
+# master interface
+class AxiMaster(object):
+    burst_size_width = 8
+
+    def __init__(self, m, name, clk, rst, datawidth=32, addrwidth=32):
+        self.m = m
+        self.name = name
+        self.clk = clk
+        self.rst = rst
+        self.datawidth = datawidth
+        self.addrwidth = addrwidth
+
+        self.waddr = AxiMasterWriteAddress(m, name, datawidth, addrwidth)
+        self.wdata = AxiMasterWriteData(m, name, datawidth, addrwidth)
+        self.raddr = AxiMasterReadAddress(m, name, datawidth, addrwidth)
+        self.rdata = AxiMasterReadData(m, name, datawidth, addrwidth)
+
+        self.seq = Seq(m, name, clk, rst)
+        self.m.add_hook(self.seq.make_always)
+
+        self.write_counters = []
+        self.read_counters = []
+
+        self._write_disabled = False
+        self._read_disabled = False
+
+    def disable_write(self):
+        self.seq(
+            self.waddr.awaddr(0),
+            self.waddr.awlen(0),
+            self.waddr.awvalid(0),
+            self.wdata.wdata(0),
+            self.wdata.wstrb(0),
+            self.wdata.wvalid(0),
+            self.wdata.wlast(0)
+        )
+        self._write_disabled = True
+
+    def disable_read(self):
+        self.seq(
+            self.raddr.araddr(0),
+            self.raddr.arlen(0),
+            self.raddr.arvalid(0),
+            self.rdata.rready(0),
+        )
+        self._read_disabled = True
+
+    def write_request(self, addr, length=1, cond=None):
+        if self._write_disabled:
+            raise TypeError('Write disabled.')
+        
+        if cond is not None:
+            self.seq.If(cond)
+
+        if self.seq.current_delay > 0:
+            raise ValueError("Delayed control is not supported.")
+
+        if isinstance(length, int) and length > 2 ** self.burst_size_width:
+            raise ValueError("length must be less than 257.")
+
+        if isinstance(length, int) and length < 1:
+            raise ValueError("length must be more than 0.")
+
+        ack = vtypes.Ors(self.waddr.awready,
+                         vtypes.Not(self.waddr.awvalid))
+
+        counter = self.m.TmpReg(self.burst_size_width, initval=0)
+        self.write_counters.append(counter)
+
+        self.seq.If(ack)(
+            self.waddr.awaddr(addr),
+            self.waddr.awlen(length - 1),
+            self.waddr.awvalid(1),
+            counter(length - 1)
+        )
+        
+        # de-assert
+        self.seq.Delay(1)(
+            self.waddr.awvalid(0)
+        )
+
+        # retry
+        self.seq.If(vtypes.Ands(self.waddr.awvalid, vtypes.Not(self.waddr.awready)))(
+            self.waddr.awvalid(self.waddr.awvalid)
+        )
+
+        return ack, counter
+
+    def write_data(self, data, counter=None, cond=None):
+        if self._write_disabled:
+            raise TypeError('Write disabled.')
+
+        if counter is None:
+            counter = self.write_counters[-1]
+        
+        if cond is not None:
+            self.seq.If(cond)
+
+        if self.seq.current_delay > 0:
+            raise ValueError("Delayed control is not supported.")
+
+        ack = vtypes.Ors(self.wdata.wready,
+                         vtypes.Not(self.wdata.wvalid))
+        last = self.m.TmpReg(initval=0)
+
+        self.seq.If(vtypes.Ands(ack, vtypes.Not(last)))(
+            self.wdata.wdata(data),
+            self.wdata.wvalid(1),
+            self.wdata.wlast(0),
+            self.wdata.wstrb(vtypes.Repeat(vtypes.Int(1,1), (self.wdata.datawidth//8))),
+            counter.dec()
+        )
+        self.seq.Then().If(counter == 0)(
+            self.wdata.wlast(1),
+            last(1)
+        )
+
+        # de-assert
+        self.seq.Delay(1)(
+            self.wdata.wvalid(0)
+        )
+        self.seq.Delay(1)(
+            self.wdata.wlast(0),
+            last(0)
+        )
+        
+        # retry
+        self.seq.If(vtypes.Ands(self.wdata.wvalid, vtypes.Not(self.wdata.wready)))(
+            self.wdata.wvalid(self.wdata.wvalid),
+            self.wdata.wlast(self.wdata.wlast),
+            last(last)
+        )
+
+        return ack, last
+
+    def read_request(self, addr, length, cond=None):
+        if self._read_disabled:
+            raise TypeError('Read disabled.')
+        
+        counter = self.m.TmpReg(self.burst_size_width, initval=0)
+        self.read_counters.append(counter)
+
+        ack = 1
+        return ack, counter
+
+    def read_data(self, counter=None, cond=None):
+        if self._read_disabled:
+            raise TypeError('Read disabled.')
+        
+        if counter is None:
+            counter = self.read_counters[-1]
+        
+        data = 0
+        valid = 1
+        last = 0
+        return data, valid, last
