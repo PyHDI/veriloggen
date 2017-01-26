fba02079dfa6719b965c2c10da6c4970c85c65c5
shtaxxx <shta.ky1018@gmail.com>
2017-01-26 16:17:20 +0900

    renamed

--- tests/extension/thread_/read/test_thread_read.py
+++ tests/extension/thread_/read/test_thread_read.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import veriloggen
-import thread_mixed
+import thread_read
 
 expected_verilog = """
 module test;
@@ -125,7 +125,7 @@ endmodule
 
 def test():
     veriloggen.reset()
-    test_module = thread_mixed.mkTest()
+    test_module = thread_read.mkTest()
     code = test_module.to_verilog()
 
     from pyverilog.vparser.parser import VerilogParser
--- tests/extension/thread_/write/test_thread_write.py
+++ tests/extension/thread_/write/test_thread_write.py
@@ -1,26 +1,22 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import veriloggen
-import thread_embeded
+import thread_write
 
 expected_verilog = """
 module test;
 
   reg CLK;
   reg RST;
+  wire [8-1:0] LED;
 
   blinkled
   uut
   (
     .CLK(CLK),
-    .RST(RST)
+    .RST(RST),
+    .LED(LED)
   );
-
-
-  initial begin
-    $dumpfile("uut.vcd");
-    $dumpvars(0, uut);
-  end
 
 
   initial begin
@@ -49,17 +45,16 @@ endmodule
 module blinkled
 (
   input CLK,
-  input RST
+  input RST,
+  output reg [8-1:0] LED
 );
 
-  reg [8-1:0] data;
-  reg enable;
-  wire ready;
-  assign ready = 1;
   reg [32-1:0] fsm;
   localparam fsm_init = 0;
   reg [32-1:0] _thread_fsm_times_0;
-  reg [32-1:0] _thread_fsm_i_1;
+  reg [32-1:0] _thread_fsm_inc_1;
+  reg [32-1:0] _thread_fsm_dump_2;
+  reg [32-1:0] _thread_fsm_i_3;
   localparam fsm_1 = 1;
   localparam fsm_2 = 2;
   localparam fsm_3 = 3;
@@ -67,46 +62,53 @@ module blinkled
   localparam fsm_5 = 5;
   localparam fsm_6 = 6;
   localparam fsm_7 = 7;
+  localparam fsm_8 = 8;
 
   always @(posedge CLK) begin
     if(RST) begin
       fsm <= fsm_init;
-      data <= 0;
-      enable <= 0;
+      LED <= 0;
     end else begin
       case(fsm)
         fsm_init: begin
           _thread_fsm_times_0 <= 10;
+          _thread_fsm_inc_1 <= 1;
+          _thread_fsm_dump_2 <= 1;
           fsm <= fsm_1;
         end
         fsm_1: begin
-          _thread_fsm_i_1 <= 0;
+          LED <= 0;
           fsm <= fsm_2;
         end
         fsm_2: begin
-          if(_thread_fsm_i_1 < _thread_fsm_times_0) begin
-            fsm <= fsm_3;
+          _thread_fsm_i_3 <= 0;
+          fsm <= fsm_3;
+        end
+        fsm_3: begin
+          if(_thread_fsm_i_3 < _thread_fsm_times_0) begin
+            fsm <= fsm_4;
+          end else begin
+            fsm <= fsm_8;
+          end
+        end
+        fsm_4: begin
+          LED <= LED + _thread_fsm_inc_1;
+          fsm <= fsm_5;
+        end
+        fsm_5: begin
+          if(_thread_fsm_dump_2) begin
+            fsm <= fsm_6;
           end else begin
             fsm <= fsm_7;
           end
         end
-        fsm_3: begin
-          data <= _thread_fsm_i_1;
-          enable <= 1;
-          fsm <= fsm_4;
-        end
-        fsm_4: begin
-          enable <= 0;
-          fsm <= fsm_5;
-        end
-        fsm_5: begin
-          if(ready) begin
-            fsm <= fsm_6;
-          end 
-        end
         fsm_6: begin
-          _thread_fsm_i_1 <= _thread_fsm_i_1 + 1;
-          fsm <= fsm_2;
+          $display("led =  %d", LED);
+          fsm <= fsm_7;
+        end
+        fsm_7: begin
+          _thread_fsm_i_3 <= _thread_fsm_i_3 + 1;
+          fsm <= fsm_3;
         end
       endcase
     end
@@ -119,7 +121,7 @@ endmodule
 
 def test():
     veriloggen.reset()
-    test_module = thread_embeded.mkTest()
+    test_module = thread_write.mkTest()
     code = test_module.to_verilog()
 
     from pyverilog.vparser.parser import VerilogParser
