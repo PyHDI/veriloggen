7187a7496bd4456d29266ce6ecbc89d2c85cd072
shtaxxx <shta.ky1018@gmail.com>
2017-02-11 18:11:20 +0900

    renamed

--- tests/extension/thread_/call_implicit_function/test_thread_call_implicit_function.py
+++ tests/extension/thread_/call_implicit_function/test_thread_call_implicit_function.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 import veriloggen
-import thread_function
+import thread_call_implicit_function
 
 expected_verilog = """
 module test;
@@ -141,7 +141,7 @@ endmodule
 
 def test():
     veriloggen.reset()
-    test_module = thread_function.mkTest()
+    test_module = thread_call_implicit_function.mkTest()
     code = test_module.to_verilog()
 
     from pyverilog.vparser.parser import VerilogParser
