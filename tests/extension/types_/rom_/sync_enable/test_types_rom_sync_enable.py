3d5ff59d22a7f35d752e6bc6635a3038cea669f8
shtaxxx <shta.ky1018@gmail.com>
2017-02-24 01:20:20 +0900

    sync_enable

---
+++ tests/extension/types_/rom_/sync_enable/test_types_rom_sync_enable.py
@@ -0,0 +1,172 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import veriloggen
+import types_rom_sync_enable
+
+expected_verilog = """
+module test;
+
+  reg CLK;
+  reg RST;
+
+  main
+  uut
+  (
+    .CLK(CLK),
+    .RST(RST)
+  );
+
+
+  initial begin
+    $dumpfile("uut.vcd");
+    $dumpvars(0, uut, CLK, RST);
+  end
+
+
+  initial begin
+    CLK = 0;
+    forever begin
+      #5 CLK = !CLK;
+    end
+  end
+
+
+  initial begin
+    RST = 0;
+    #100;
+    RST = 1;
+    #100;
+    RST = 0;
+    #100000;
+    $finish;
+  end
+
+
+endmodule
+
+
+
+module main
+(
+  input CLK,
+  input RST
+);
+
+  reg [4-1:0] addr;
+  reg enable;
+  wire [4-1:0] myrom_addr;
+  assign myrom_addr = addr;
+  wire myrom_enable;
+  assign myrom_enable = enable;
+  wire [8-1:0] myrom_val;
+
+  myrom
+  myrom
+  (
+    .CLK(CLK),
+    .addr(myrom_addr),
+    .enable(myrom_enable),
+    .val(myrom_val)
+  );
+
+
+  always @(posedge CLK) begin
+    if(RST) begin
+      enable <= 0;
+      addr <= 0;
+    end else begin
+      enable <= !enable;
+      if(enable) begin
+        addr <= addr + 1;
+        $display("addr=%d rdata=%d", addr, myrom_val);
+      end 
+    end
+  end
+
+
+endmodule
+
+
+
+module myrom
+(
+  input CLK,
+  input [4-1:0] addr,
+  input enable,
+  output reg [8-1:0] val
+);
+
+
+  always @(posedge CLK) begin
+    if(enable) begin
+      case(addr)
+        0: begin
+          val <= 0;
+        end
+        1: begin
+          val <= 1;
+        end
+        2: begin
+          val <= 4;
+        end
+        3: begin
+          val <= 9;
+        end
+        4: begin
+          val <= 16;
+        end
+        5: begin
+          val <= 25;
+        end
+        6: begin
+          val <= 36;
+        end
+        7: begin
+          val <= 49;
+        end
+        8: begin
+          val <= 64;
+        end
+        9: begin
+          val <= 81;
+        end
+        10: begin
+          val <= 100;
+        end
+        11: begin
+          val <= 121;
+        end
+        12: begin
+          val <= 144;
+        end
+        13: begin
+          val <= 169;
+        end
+        14: begin
+          val <= 196;
+        end
+        15: begin
+          val <= 225;
+        end
+      endcase
+    end 
+  end
+
+
+endmodule
+"""
+
+
+def test():
+    veriloggen.reset()
+    test_module = types_rom_sync_enable.mkTest()
+    code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    parser = VerilogParser()
+    expected_ast = parser.parse(expected_verilog)
+    codegen = ASTCodeGenerator()
+    expected_code = codegen.visit(expected_ast)
+
+    assert(expected_code == code)
---
+++ tests/extension/types_/rom_/sync_enable/types_rom_sync_enable.py
@@ -0,0 +1,78 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+import os
+import math
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))))
+
+from veriloggen import *
+import veriloggen.types.rom as rom
+
+
+def mkMain(n=128, datawidth=32, numports=2):
+    m = Module('main')
+
+    clk = m.Input('CLK')
+    rst = m.Input('RST')
+
+    width = 4
+    addr = m.Reg('addr', width, initval=0)
+    values = [ i * i for i in range(2 ** width) ]
+    enable = m.Reg('enable', initval=0)
+
+    myrom = rom.SyncROM(m, 'myrom', clk, addr, values, datawidth=8, enable=enable)
+
+    seq = Seq(m, 'seq', clk, rst)
+
+    seq(
+        enable(Not(enable)),
+    )
+    seq.If(enable)(
+        addr.inc(),
+        Display('addr=%d rdata=%d', addr, myrom.rdata)
+    )
+
+    return m
+
+
+def mkTest():
+    m = Module('test')
+
+    # target instance
+    main = mkMain()
+
+    # copy paras and ports
+    params = m.copy_params(main)
+    ports = m.copy_sim_ports(main)
+
+    clk = ports['CLK']
+    rst = ports['RST']
+
+    uut = m.Instance(main, 'uut',
+                     params=m.connect_params(main),
+                     ports=m.connect_ports(main))
+
+    simulation.setup_waveform(m, uut, m.get_vars())
+    simulation.setup_clock(m, clk, hperiod=5)
+    init = simulation.setup_reset(m, rst, m.make_reset(), period=100)
+
+    init.add(
+        Delay(1000 * 100),
+        Systask('finish'),
+    )
+
+    return m
+
+if __name__ == '__main__':
+    test = mkTest()
+    verilog = test.to_verilog('tmp.v')
+    print(verilog)
+
+    sim = simulation.Simulator(test)
+    rslt = sim.run()
+    print(rslt)
+
+    # sim.view_waveform()
