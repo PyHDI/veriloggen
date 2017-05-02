6787b47206fd980887b31520e3e8938f8806d10d
shtaxxx <shta.ky1018@gmail.com>
2017-05-03 00:21:22 +0900

    EmbeddedCode

---
+++ tests/core/embeddedcode/embeddedcode.py
@@ -0,0 +1,42 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+import os
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))
+
+from veriloggen import *
+
+
+def mkLed():
+    m = Module('blinkled')
+    clk = m.Input('CLK')
+    rst = m.Input('RST')
+    led = m.OutputReg('LED', 8, initval=0)
+
+    code = m.EmbeddedCode("""
+// Embedded code
+reg [31:0] count;
+always @(posedge CLK) begin
+  if(RST) begin
+    count <= 0;
+    LED <= 0;        
+  end else begin
+    if(count == 1024 - 1) begin
+      count <= 0;
+      LED <= LED + 1;
+    end else begin
+      count <= count + 1;
+    end 
+  end
+end
+""")
+
+    return m
+
+if __name__ == '__main__':
+    led = mkLed()
+    verilog = led.to_verilog()
+    print(verilog)
---
+++ tests/core/embeddedcode/test_embeddedcode.py
@@ -0,0 +1,50 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import veriloggen
+import embeddedcode
+
+expected_verilog = """
+
+module blinkled
+(
+  input CLK,
+  input RST,
+  output reg [8-1:0] LED
+);
+
+
+    // Embedded code
+    reg [31:0] count;
+    always @(posedge CLK) begin
+      if(RST) begin
+        count <= 0;
+        LED <= 0;        
+      end else begin
+        if(count == 1024 - 1) begin
+          count <= 0;
+          LED <= LED + 1;
+        end else begin
+          count <= count + 1;
+        end 
+      end
+    end
+
+
+endmodule
+
+"""
+
+def test():
+    veriloggen.reset()
+    test_module = embeddedcode.mkLed()
+    code = test_module.to_verilog()
+
+    from pyverilog.vparser.parser import VerilogParser
+    from pyverilog.ast_code_generator.codegen import ASTCodeGenerator
+    #parser = VerilogParser()
+    #expected_ast = parser.parse(expected_verilog)
+    #codegen = ASTCodeGenerator()
+    #expected_code = codegen.visit(expected_ast)
+    expected_code = expected_verilog
+
+    assert(expected_code == code)
