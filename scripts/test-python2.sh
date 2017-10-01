bfbbf1895f92b7d2ac67f3675e3415190e553a75
shtaxxx <shta.ky1018@gmail.com>
2017-10-01 12:25:35 +0900

    email address is removed

--- scripts/test-python2.sh
+++ scripts/test-python2.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 set -e
 
-mkdir 2.7
-cd 2.7
+mkdir python
+cd python
 virtualenv --python=python .
 source bin/activate
+
 git clone https://github.com/PyHDI/veriloggen.git
 cd veriloggen
 python setup.py install
 pip install pytest pytest-pythonpath
 mv veriloggen veriloggen.old
-make run -C tests PYTHON=python
-make clean -C tests
-make run -C examples PYTHON=python
-make clean -C examples
-make test PYTHON=python
+
+python -m pytest -vv .
+
 mv veriloggen.old veriloggen
 cd ..
 deactivate
--- scripts/test-python3.sh
+++ scripts/test-python3.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 set -e
 
-mkdir 3.5
-cd 3.5
+mkdir python3
+cd python3
 virtualenv --python=python3 .
 source bin/activate
+
 git clone https://github.com/PyHDI/veriloggen.git
 cd veriloggen
-python3 setup.py install
+python setup.py install
 pip install pytest pytest-pythonpath
 mv veriloggen veriloggen.old
-make run -C tests
-make clean -C tests
-make run -C examples
-make clean -C examples
-make test
+
+python -m pytest -vv .
+
 mv veriloggen.old veriloggen
 cd ..
 deactivate
