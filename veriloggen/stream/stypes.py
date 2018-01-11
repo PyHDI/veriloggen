834d6fce34a8b0049da7fd4184db56836a7bf4ab
shtaxxx <shta.ky1018@gmail.com>
2018-01-11 15:59:23 +0900

    Bugs of Max and Min are fixed.

--- veriloggen/stream/stypes.py
+++ veriloggen/stream/stypes.py
@@ -1618,14 +1618,14 @@ def Max(*args):
     if len(args) == 1:
         return args[0]
     initval = args[0]
-    return op_tree(lambda x, y: Mux(x > y, x, y), initval, args[1:])
+    return op_tree(lambda x, y: Mux(x > y, x, y), initval, *args[1:])
 
 
 def Min(*args):
     if len(args) == 1:
         return args[0]
     initval = args[0]
-    return op_tree(lambda x, y: Mux(x < y, x, y), initval, args[1:])
+    return op_tree(lambda x, y: Mux(x < y, x, y), initval, *args[1:])
 
 
 def Average(*args):
