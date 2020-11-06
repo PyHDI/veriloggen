3cc8a7617c0c099aa562e091136e7d96d616db98
shtaxxx <shta.ky1018@gmail.com>
2020-11-07 01:52:34 +0900

    New test for SyncRAM with enable.

---
+++ tests/extension/types_/ram_/sync_with_enable/types_ram_sync_with_enable.py
@@ -0,0 +1,107 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+import os
+import math
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))))))
+
+from veriloggen import *
+import veriloggen.types.ram as ram
+
+
+def mkMain(n=128, datawidth=32, numports=2):
+    m = Module('main')
+
+    clk = m.Input('CLK')
+    rst = m.Input('RST')
+
+    addrwidth = int(math.log(n, 2)) * 2
+
+    myram = ram.SyncRAM(m, 'myram', clk, datawidth, addrwidth, 2, with_enable=True)
+
+    seq = Seq(m, 'seq', clk, rst)
+
+    enable = 1
+
+    # write
+    waddr = m.Reg('waddr', 32, initval=0)
+    wdata = m.Reg('wdata', 32, initval=0)
+    wenable = waddr < 16
+
+    myram.connect(0, waddr, wdata, wenable, enable)
+    
+    seq.If(wenable)(
+        waddr.inc(),
+        wdata.inc()
+    )
+    
+
+    # read
+    raddr = m.Reg('raddr', 32, initval=0)
+    renable = make_condition(seq.Prev(1, delay=4, initval=0), raddr < 16)
+   
+    myram.connect(1, raddr, 0, 0, enable)
+    
+    seq.If(renable)(
+        raddr.inc(),
+    )
+
+    rdata = myram.rdata(1)
+    rvalid = seq.Prev(renable, 1)
+
+    sum = m.Reg('sum', 32, initval=0)
+
+    seq.If(rvalid)(
+        sum(sum + rdata)
+    )
+
+    seq.Then().Delay(1)(
+        Systask('display', "sum=%d", sum)
+    )
+
+    seq.make_always()
+
+    return m
+
+
+def mkTest():
+    m = Module('test')
+
+    # target instance
+    main = mkMain()
+
+    # copy paras and ports
+    params = m.copy_params(main)
+    ports = m.copy_sim_ports(main)
+
+    clk = ports['CLK']
+    rst = ports['RST']
+
+    uut = m.Instance(main, 'uut',
+                     params=m.connect_params(main),
+                     ports=m.connect_ports(main))
+
+    simulation.setup_waveform(m, uut, m.get_vars())
+    simulation.setup_clock(m, clk, hperiod=5)
+    init = simulation.setup_reset(m, rst, m.make_reset(), period=100)
+
+    init.add(
+        Delay(1000 * 100),
+        Systask('finish'),
+    )
+
+    return m
+
+if __name__ == '__main__':
+    test = mkTest()
+    verilog = test.to_verilog('tmp.v')
+    print(verilog)
+
+    sim = simulation.Simulator(test)
+    rslt = sim.run()
+    print(rslt)
+
+    # sim.view_waveform()
