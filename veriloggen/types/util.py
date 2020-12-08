e7b981d0d264b0d6541b7f87dcd0ed92d3865f3c
shtaxxx <shta.ky1018@gmail.com>
2020-12-08 23:40:34 +0900

    types.util.add_cond

--- veriloggen/types/util.py
+++ veriloggen/types/util.py
@@ -61,3 +61,15 @@ def add_mux(targ, cond, value):
             vtypes.Mux(cond, value, prev_value))
         targ.module.remove(prev_assign)
         targ.module.append(prev_assign)
+
+
+def add_cond(targ, cond, value):
+    prev_assign = targ._get_assign()
+    if not prev_assign:
+        targ.assign(vtypes.Mux(cond, value, 1))
+    else:
+        prev_value = prev_assign.statement.right
+        prev_assign.overwrite_right(
+            vtypes.Ands(vtypes.Mux(cond, value, 1), prev_value))
+        targ.module.remove(prev_assign)
+        targ.module.append(prev_assign)
