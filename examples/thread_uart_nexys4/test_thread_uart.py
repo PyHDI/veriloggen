784b68a80a4e896298df578fdb9b268a7b4ce6e3
shtaxxx <shta.ky1018@gmail.com>
2022-03-06 15:23:41 +0900

    Rename

--- examples/thread_uart_nexys4/test_thread_uart.py
+++ examples/thread_uart_nexys4/test_thread_uart.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import veriloggen
-import thread_uart_nexys4
+import thread_uart
 
 
 def test(request):
@@ -10,7 +10,7 @@ def test(request):
 
     simtype = request.config.getoption('--sim')
 
-    rslt = thread_uart_nexys4.run(filename=None, simtype=simtype)
+    rslt = thread_uart.run(filename=None, simtype=simtype)
 
     verify_rslt = rslt.splitlines()[-1]
     assert(verify_rslt == '# verify: PASSED')
