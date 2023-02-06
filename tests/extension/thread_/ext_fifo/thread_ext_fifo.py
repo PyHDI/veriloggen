e7db2919d900debd23f548845b5f635b407ed231
shtaxxx <shta.ky1018@gmail.com>
2023-02-06 11:10:58 +0900

    Added new example/test codes for ExtRAM/ExtFIFO and RAM/FIFO with Tmp names.

---
+++ tests/extension/thread_/ext_fifo/thread_ext_fifo.py
@@ -0,0 +1,107 @@
+from __future__ import absolute_import
+from __future__ import print_function
+import sys
+import os
+
+# the next line can be removed after installation
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(
+    os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))))
+
+from veriloggen import *
+import veriloggen.thread as vthread
+
+datawidth = 32
+addrwidth = 4
+
+
+def mkLed():
+    m = Module('blinkled')
+    clk = m.Input('CLK')
+    rst = m.Input('RST')
+
+    myfifo = vthread.ExtFIFO(m, 'myfifo', clk, rst, datawidth, addrwidth)
+
+    def blink(times):
+        for i in range(times):
+            wdata = i
+            myfifo.enq(wdata)
+            print('wdata = %d' % wdata)
+
+        sum = 0
+        for i in range(times):
+            rdata = myfifo.deq()
+            sum += rdata
+            print('rdata = %d' % rdata)
+
+        print('sum = %d' % sum)
+
+        if vthread.verilog.Eql(sum, (times - 1) * times // 2):
+            print('# verify: PASSED')
+        else:
+            print('# verify: FAILED')
+
+        vthread.finish()
+
+    th = vthread.Thread(m, 'th_blink', clk, rst, blink)
+    fsm = th.start(13)
+
+    return m
+
+
+def mkTest():
+    m = Module('test')
+
+    # target instance
+    led = mkLed()
+
+    # copy paras and ports
+    params = m.copy_params(led)
+    ports = m.copy_sim_ports(led)
+
+    clk = ports['CLK']
+    rst = ports['RST']
+
+    test_fifo = vthread.FIFO(m, 'test_fifo', clk, rst, datawidth, addrwidth)
+    test_fifo.connect_enq_rtl(ports['myfifo_enq'], ports['myfifo_wdata'],
+                              ports['myfifo_full'], ports['myfifo_almost_full'])
+    test_fifo.connect_deq_rtl(ports['myfifo_deq'], ports['myfifo_rdata'],
+                              ports['myfifo_empty'], ports['myfifo_almost_empty'])
+
+    uut = m.Instance(led, 'uut',
+                     params=m.connect_params(led),
+                     ports=m.connect_ports(led))
+
+    # vcd_name = os.path.splitext(os.path.basename(__file__))[0] + '.vcd'
+    # simulation.setup_waveform(m, uut, dumpfile=vcd_name)
+    simulation.setup_clock(m, clk, hperiod=5)
+    init = simulation.setup_reset(m, rst, m.make_reset(), period=100)
+
+    init.add(
+        Delay(10000),
+        Systask('finish'),
+    )
+
+    return m
+
+
+def run(filename='tmp.v', simtype='iverilog', outputfile=None):
+
+    if outputfile is None:
+        outputfile = os.path.splitext(os.path.basename(__file__))[0] + '.out'
+
+    test = mkTest()
+
+    if filename is not None:
+        test.to_verilog(filename)
+
+    sim = simulation.Simulator(test, sim=simtype)
+    rslt = sim.run(outputfile=outputfile)
+    lines = rslt.splitlines()
+    if simtype == 'verilator' and lines[-1].startswith('-'):
+        rslt = '\n'.join(lines[:-1])
+    return rslt
+
+
+if __name__ == '__main__':
+    rslt = run(filename='tmp.v')
+    print(rslt)
